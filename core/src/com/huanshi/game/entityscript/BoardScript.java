3417894744b9aff7a909c0531b71d074239cc260
unknown <dudeqing@live.com>
2017-04-26 16:51:27 +0800

    平台移动

--- core/src/com/huanshi/game/entityscript/BoardScript.java
+++ core/src/com/huanshi/game/entityscript/BoardScript.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.huanshi.game.EntityScript;
 import com.huanshi.game.GameConfig;
 import com.huanshi.game.GameVar;
+import com.huanshi.game.GlobalInlineVar;
 import com.huanshi.game.components.PhysicsComponent;
 import com.huanshi.game.components.TransformComponent;
 import com.huanshi.game.support.GlobalInline;
@@ -13,12 +14,16 @@ import com.huanshi.game.tools.MapperTools;
 /**
  * 跳台脚本
  * 
+ * 最高难度 所有跳台都可以移动
+ * 
  * @author D
  * @date 2016年12月14日
  */
 public class BoardScript extends EntityScript{
 	
-	public Entity sensor;
+	public Entity sensor; // 传感器
+	
+	public float speed = 100; // 移动速度 
 	
 	@Override
 	public void update(float deltaTime) {
@@ -26,9 +31,9 @@ public class BoardScript extends EntityScript{
 		TransformComponent transformComponent = MapperTools.transformCM.get(entity);
 		Vector2 position = transformComponent.position;
 		
-		Vector2 jumpBoardPosition = GlobalInline.instance.get("jumpBoardPosition");
+		Vector2 jumpBoardPosition = GlobalInline.instance.get(GlobalInlineVar.jumpBoardPosition);
 		
-		if(jumpBoardPosition !=null && position.y < jumpBoardPosition.y){
+		if(jumpBoardPosition !=null && position.y < jumpBoardPosition.y){ // 已经超过
 			
 			if(position.y < GameVar.gameViewport.getCamera().position.y - GameConfig.height/2){
 				if(entity != null){
@@ -47,6 +52,17 @@ public class BoardScript extends EntityScript{
 					sensor = null;
 				}
 			}
+		}
+		else{
+			
+			if((position.x + transformComponent.getWidth()/2 >= GameConfig.width && speed > 0) // 右移动超出屏幕
+					|| (position.x - transformComponent.getWidth()/2 <= 0 && speed < 0)){ // 左移动超出屏幕
+				speed = -speed;
+			}
+			
+			PhysicsComponent physicsComponent = MapperTools.physicsCM.get(entity);
+			physicsComponent.rigidBody.setAwake(true);
+			physicsComponent.rigidBody.setLinearVelocity(speed, 0);
 		}
 	}
 }
